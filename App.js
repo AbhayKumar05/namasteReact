3c06839943985328c8b63d91c1cb9e9411c2a34b
AbhayKumar05 <abhaylagah01@gmail.com>
2025-10-30 18:50:12 +0000

    Ep.1

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
-const heading = React.createElement(
-    "h1", {}, "Hello from React!"
+
+/**
+ * 
+ * <div id="parent">
+    *  <div id="child">
+    *      <h1>Hello from React!</h1>
+    *     <h2>Hello from React!</h2>
+    *  </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+    "div",{ id: "parent"},
+    React.createElement(
+        "div",{ id: "child"},
+        [React.createElement("h1", {}, "Im a h1 tag inside child div"), 
+        React.createElement("h2", {}, "Im a h2 tag inside child div")]
+    )
 );
+
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+    root.render(parent);
             
