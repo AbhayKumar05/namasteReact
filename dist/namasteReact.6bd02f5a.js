c26de708fb5df8be39c4ba04e9317b3b61da8417
AbhayKumar05 <abhaylagah01@gmail.com>
2025-11-01 21:39:57 +0000

    Episode-02 | Igniting our App

---
+++ dist/namasteReact.6bd02f5a.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * <div id="parent">
+    *  <div id="child">
+    *      <h1>Hello from React!</h1>
+    *     <h2>Hello from React!</h2>
+    *  </div>
+ * </div>
+ */ const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "Im a h1 tag inside child div"),
+    React.createElement("h2", {}, "Im a h2 tag inside child div")
+]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=namasteReact.6bd02f5a.js.map
