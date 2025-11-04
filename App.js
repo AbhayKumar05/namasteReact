48cfc233b86c86a105b603e38b2be88ec5731e95
AbhayKumar05 <abhaylagah01@gmail.com>
2025-11-04 23:49:17 +0000

    Episode-03 | Laying the foundation

--- App.js
+++ App.js
@@ -13,6 +13,23 @@ import ReactDOM from "react-dom/client";
 
 const heading = <h1 className="heading" tabIndex={1}>Hello JSX</h1>;
 
+const Title = () => (
+    <h1 className="title" tabIndex={2}>
+        Hello from Title Component
+    </h1>
+);
+
+// Functional Component or component composition 
+const HeadingComponent = () => {
+    return ( <div id="parent">
+        <div id="child">
+            {heading}
+            {Title()} 
+            <Title />
+        </div>
+    </div>);
+}
+
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+    root.render(<headingComponent />);
             
