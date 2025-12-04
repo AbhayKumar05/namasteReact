eb780e73b76d9cb27a8082645b53a1a6e0492380
AbhayKumar05 <abhaylagah01@gmail.com>
2025-12-04 22:38:26 +0000

    Episode-15 | NetflixGPT - Building the Core

--- netflix-gpt/src/components/VideoBackground.js
+++ netflix-gpt/src/components/VideoBackground.js
@@ -10,7 +10,7 @@ const VideoBackground = ({movieID}) => {
     <div className="w-screen">
       <iframe 
         className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/"+ videoTrailer?.key} 
+        src={"https://www.youtube.com/embed/"+ videoTrailer?.key + "?autoplay=1&mute=1&controls=0&loop=1&playlist=" + videoTrailer?.key} 
         title="Elemental | Official Trailer" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         referrerpolicy="strict-origin-when-cross-origin">
